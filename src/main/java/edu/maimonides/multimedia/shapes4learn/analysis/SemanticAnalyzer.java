ba3356df350d04a4b969640ab303e1f5b69705bd
Gorino Hernan <gorino.miguel@maimonides.edu>
2015-12-12 06:33:53 -0300

    Semantico
    - Se corrige como se detecta y se informa id duplicado
    - Se agrega logica para recorrer el sub ast de la expression
    - Se intenta hacer la logica para resolver la operacion que se encuentra
    en la expression (no se pudo terminar)

--- src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SemanticAnalyzer.java
+++ src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SemanticAnalyzer.java
@@ -2,6 +2,7 @@ package edu.maimonides.multimedia.shapes4learn.analysis;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import edu.maimonides.multimedia.shapes4learn.model.AST;
 
@@ -15,6 +16,9 @@ import edu.maimonides.multimedia.shapes4learn.model.AST;
  */
 public class SemanticAnalyzer {
 
+	String infixNotation = "";
+	String postfixNotation = "";
+
 	public SemanticAnalyzer() {
 	}
 
@@ -27,31 +31,40 @@ public class SemanticAnalyzer {
 
 		List<AST> listChildren = ast.listChildren();
 		boolean errorSemantico = false;
+		Integer lineNumber = 1;
 
 		System.out.println("\nFunciones: \n");
 		for (AST astTemp : listChildren) {
 
-			System.out.println(astTemp.getValue());
+			// System.out.println(astTemp.getValue());
 			switch (astTemp.getValue()) {
 			case "create":
-				errorSemantico = checkSemanticCreate(astTemp, idList);
+				errorSemantico = checkSemanticCreate(astTemp, idList,
+						lineNumber);
 				break;
 			case "setcolor":
-				errorSemantico = checkSemanticSetColor(astTemp, idList);
+				errorSemantico = checkSemanticSetColor(astTemp, idList,
+						lineNumber);
 				break;
 			case "setbase":
-				errorSemantico = checkSemanticSetBase(astTemp, idList);
+				errorSemantico = checkSemanticSetBase(astTemp, idList,
+						lineNumber);
 				break;
 			case "setheight":
-				errorSemantico = checkSemanticSetHeight(astTemp, idList);
+				errorSemantico = checkSemanticSetHeight(astTemp, idList,
+						lineNumber);
 				break;
 			case "setradius":
-				errorSemantico = checkSemanticSetRadius(astTemp, idList);
+				errorSemantico = checkSemanticSetRadius(astTemp, idList,
+						lineNumber);
 				break;
 			case "setposition":
-				errorSemantico = checkSemanticSetPosition(astTemp, idList);
+				errorSemantico = checkSemanticSetPosition(astTemp, idList,
+						lineNumber);
 				break;
 			}
+
+			lineNumber++;
 		}
 
 		if (errorSemantico) {
@@ -61,110 +74,302 @@ public class SemanticAnalyzer {
 		return ast;
 	}
 
-	private boolean checkSemanticSetPosition(AST astTemp, List<String> idList2) {
+	private boolean checkSemanticSetPosition(AST astTemp, List<String> idList,
+			Integer lineNumber) {
 		// Chequeo si las expresiones son iguales o menores a cero, si pasa
 		// ésto, arroja error. Si no, reemplazar el resultado en el AST.
 		// En este caso, son dos expresiones a verificar.
 
-		// Chequeo si existe el ID en la lista de IDs existentes y, si no
-		// existe, arroja error
+		// 1- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
 		for (String id : idList) {
 			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
-				return false;
-			}
-		}
-		return true;
-
-	}
-
-	private boolean checkSemanticSetRadius(AST astTemp, List<String> idList2) {
-		// Chequeo si la expresión es igual o menor a cero, si pasa ésto, arroja
-		// error. Si no, reemplazar el resultado en el AST.
-
-		// Chequeo si existe el ID en la lista de IDs existentes y, si no
-		// existe, arroja error
-		for (String id : idList) {
-			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean checkSemanticSetHeight(AST astTemp, List<String> idList2) {
-		// Chequeo si la expresión es igual o menor a cero, si pasa ésto, arroja
-		// error. Si no, reemplazar el resultado en el AST.
-
-		// Chequeo si existe el ID en la lista de IDs existentes y, si no
-		// existe, arroja error
-		for (String id : idList) {
-			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean checkSemanticSetBase(AST astTemp, List<String> idList2) {
-		// Chequeo si la expresión es igual o menor a cero, si pasa ésto, arroja
-		// error. Si no, reemplazar el resultado en el AST.
-
-		// Chequeo si existe el ID en la lista de IDs existentes y, si no
-		// existe, arroja error
-		for (String id : idList) {
-			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean checkSemanticSetColor(AST astTemp, List<String> idList2) {
-		// Chequeo si existe el ID en la lista de IDs existentes y, si no
-		// existe, arroja error
-		for (String id : idList) {
-			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private boolean checkSemanticCreate(AST astTemp, List<String> idList) {
-
-		// Chequeo si existe el ID en la lista de IDs existentes. Si existe,
-		// debe arrojar error y, si no
-		// existe, lo agrego
-		for (String id : idList) {
-			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
 				return true;
 			}
 		}
+
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ "es correcta.");
+		return false;
+	}
+
+	private boolean checkSemanticSetRadius(AST astTemp, List<String> idList,
+			Integer lineNumber) {
+		// Chequeo si la expresión es igual o menor a cero, si pasa ésto, arroja
+		// error. Si no, reemplazar el resultado en el AST.
+
+		// 1- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
+		for (String id : idList) {
+			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
+				return true;
+			}
+		}
+
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ "es correcta.");
+		return false;
+	}
+
+	private boolean checkSemanticSetHeight(AST astTemp, List<String> idList,
+			Integer lineNumber) {
+		// 1- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
+		for (String id : idList) {
+			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
+				return true;
+			}
+		}
+
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ "es correcta.");
+		return false;
+	}
+  
+	private boolean checkSemanticSetBase(AST astTemp, List<String> idList,
+			Integer lineNumber) {
+		// 1- Se recorre el AST de la EXPRESSION
+		visitAST(astTemp.getChild(0));
+
+		// 2- Se pasa de polaca a polaca inversa
+		//postfixNotation = convertNotation(infixNotation);
+
+		// 3- Se revuelve la EXPRESSION
+		// System.out.println("Polaca " + polacaNotation);
+		resolveExpression(postfixNotation.trim());
+
+		// 3- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
+		for (String id : idList) {
+			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
+				return true;
+			}
+		}
+
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ "es correcta.");
+		return false;
+	}
+
+	/*private String convertNotation(String infixNotation) {
+		infixNotation = infixNotation.replace(" ","");
+		System.out.println("imprimo infixNotation " + infixNotation );
+		String expr = depurar(infixNotation);
+		System.out.println("imprimo infixNotation 2 " + infixNotation );
+		String[] arrayInfix = expr.split(" ");
+
+		// Declaración de las pilas
+		Stack<String> E = new Stack<String>(); // Pila entrada
+		Stack<String> P = new Stack<String>(); // Pila temporal para operadores
+		Stack<String> S = new Stack<String>(); // Pila salida
+
+		// Añadir la array a la Pila de entrada (E)
+		for (int i = arrayInfix.length - 1; i >= 0; i--) {
+			System.out.println("Valor i " + i);
+			E.push(arrayInfix[i]);
+			System.out.println("Valor E " + E.peek()); 
+		}
+
+		try {
+			// Algoritmo Infijo a Postfijo
+			while (!E.isEmpty()) {
+				switch (pref(E.peek())) {
+				case 1:
+					System.out.println("Valor pila 1 " + E.peek());
+					P.push(E.pop());
+					break;
+				case 3:
+				case 4:
+					while (pref(P.peek()) >= pref(E.peek())) { 
+						System.out.println("Valor pila 2 " + P.peek());
+						S.push(P.pop());
+					}
+					System.out.println("Valor pila 3 " + E.peek());
+					P.push(E.pop());
+					break;
+				case 2:
+					while (!P.peek().equals("(")) {
+						System.out.println("Valor pila 4 " + P.peek());
+						S.push(P.pop());
+					}
+					P.pop();
+					E.pop();
+					break;
+				default:
+					System.out.println("Valor pila 5 " + E.peek());
+					S.push(E.pop());
+				}
+			}
+
+			// Eliminacion de 'basura' en la expression
+			// String infix = expr.replace(" ", "");
+			System.out.println("Substring " + S.toString());
+			expr = S.toString().replaceAll("[\\]\\[,]", "");
+
+			// Mostrar resultados:
+			// System.out.println("Expresion Infija: " + infix);
+			System.out.println("Expresion Postfija: " + expr);
+
+		} catch (Exception ex) {
+			System.out.println("Error en la EXPRESSION");
+			System.err.println(ex);
+		}
+
+		return expr;
+	}*/
+
+	private boolean checkSemanticSetColor(AST astTemp, List<String> idList,
+			Integer lineNumber) {
+		// 1- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
+		for (String id : idList) {
+			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
+				return true;
+			}
+		}
+
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ " es correcta.");
+		return false;
+	}
+
+	private boolean checkSemanticCreate(AST astTemp, List<String> idList,
+			Integer lineNumber) {
+
+		// 1- Se valida si existe el ID en la lista de IDs existentes.
+		// Si existe, debe arrojar error y, si no existe, se agrega
+		for (String id : idList) {
+			if (id.equalsIgnoreCase(astTemp.getChild(1).getValue())) {
+				// ID EXISTENTE, se imprime el error y se devuelve false
+				System.out.println("Se ha detectado un error en la linea #"
+						+ lineNumber + " el ID "
+						+ astTemp.getChild(1).getValue() + " ya existe.");
+				return true;
+			}
+		}
+
+		idList.add(astTemp.getChild(1).getValue());
+		System.out.println("La semantica de la linea #" + lineNumber
+				+ " es correcta.");
 		return false;
 	}
 
 	private void visitAST(AST ast) {
-		// Metodo que recorre el AST y va imprimiendo por pantalla
+		// Metodo que recorre el AST de la EXPRESSION y la rearma en notacion
+		// polaca inversa
 
-		if (ast.getValue().equals("create")
-				|| ast.getValue().equals("setcolor")
-				|| ast.getValue().equals("setbase")
-				|| ast.getValue().equals("setheight")
-				|| ast.getValue().equals("setposition")
-				|| ast.getValue().equals("setradius")) {
-			System.out
-					.println("Representación árbol: " + ast.getValue() + "\n");
-		}
-		// Imprimo al padre
-		System.out.println(ast.getValue());
+		// Se va rearmando EXPRESSION
+		infixNotation = infixNotation + " " + ast.getValue();
 
-		// Obtengo una lista de los hijos de la raiz
+		// Se ontiene una lista de los hijos de la raiz
 		List<AST> listChildren = ast.listChildren();
 
 		for (AST tmp : listChildren) {
-			// El hijo tiene hijos y por cada uno invoco de nuevo el metodo para
+			// El hijo tiene hijos y por cada uno se invoca de nuevo el metodo
+			// para
 			// imprimirlos
 			visitAST(tmp);
 		}
 	}
+
+	private void resolveExpression(String infixNotation) {
+
+		// Se limpia la expression
+		System.out.println("Polaquita " + infixNotation);
+		String[] post = infixNotation.split(" ");
+
+		// Declaración de las pilas
+		Stack<String> E = new Stack<String>(); // Pila entrada
+		Stack<String> P = new Stack<String>(); // Pila de operandos
+
+		// Añadir post (array) a la Pila de entrada
+		for (int i = post.length - 1; i >= 0; i--) {
+			E.push(post[i]);
+		}
+
+		// Algoritmo de Evaluación Expression
+		String operadores = "+-*/%";
+		while (!E.isEmpty()) {
+			if (operadores.contains("" + E.peek())) {
+				P.push(evaluar(E.pop(), P.pop(), P.pop()) + "");
+			} else {
+				P.push(E.pop());
+			}
+		}
+
+		// Mostrar resultados:
+		System.out.println("Expresion: " + infixNotation);
+		System.out.println("Resultado: " + P.peek());
+
+	}
+
+	private static int evaluar(String op, String n2, String n1) {
+		int num1 = Integer.parseInt(n1);
+		int num2 = Integer.parseInt(n2);
+		if (op.equals("+"))
+			return (num1 + num2);
+		if (op.equals("-"))
+			return (num1 - num2);
+		if (op.equals("*"))
+			return (num1 * num2);
+		if (op.equals("/"))
+			return (num1 / num2);
+		if (op.equals("%"))
+			return (num1 % num2);
+		return 0;
+	}
+
+	private static String depurar(String s) {
+		// Elimina espacios en blanco
+		s = s.replaceAll("\\s+", "");
+		s = "(" + s + ")";
+		String simbols = "+-*/()";
+		String str = "";
+
+		// Deja espacios entre operadores
+		for (int i = 0; i < s.length(); i++) {
+			if (simbols.contains("" + s.charAt(i))) {
+				str += " " + s.charAt(i) + " ";
+			} else
+				str += s.charAt(i);
+		}
+		return str.replaceAll("\\s+", " ").trim();
+	}
+
+	// Jerarquia de los operadores
+	private static int pref(String op) {
+		int prf = 99;
+		if (op.equals("^"))
+			prf = 5;
+		if (op.equals("*") || op.equals("/"))
+			prf = 4;
+		if (op.equals("+") || op.equals("-"))
+			prf = 3;
+		if (op.equals(")"))
+			prf = 2;
+		if (op.equals("("))
+			prf = 1;
+		return prf;
+	}
+
 }
